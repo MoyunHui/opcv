6dabadd9ad844e63039e748f17d7c1ebd9af1e19
MoyunHui <1271566643@qq.com>
2018-07-11 14:56:56 +0800

    edit traffic.py

--- traffic.py
+++ traffic.py
@@ -4,24 +4,20 @@ import time
 import datetime
 import threading
 
-global timer
-global proc
+time = 0
 
-
-def fun_timer(go_ms):
-    print 'hello'
-    proc = 0
-
-    timer = threading.Timer(go_ms, fun_timer)
+def fun_timer():
+    global time
+    time = time + 1
+    timer = threading.Timer(1, fun_timer)
     timer.start()
 
-   
-
-def detection(road):
+def detection(road, time_road):
+    global time
     car_num = 0
     start_flag = 0
     cap = cv2.VideoCapture(road)
-
+    
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     fgbg = cv2.createBackgroundSubtractorMOG2()
     while (1):
@@ -48,17 +44,27 @@ def detection(road):
                         start_flag = 0
                         car_num = car_num + 1
                         print "car_num = ", car_num
-        if ch_flag == 1:
+        if time >= time_road:
+            print "time_road = ", time_road
+            print "road = ", road
+            time = 0
             cap.release()
             return car_num
 
 def main():
-    fun_timer(20)
+    timer = threading.Timer(1, fun_timer)
+    timer.start()
+    
+    time_road0, time_road1, time_road2, time_road3 = [2, 3, 4, 5]
     while True:
-        road0_num = detection(0)
-        road1_num = detection(1)
-        road2_num = detection(2)
-        road3_num = detection(3)
+        road0_num = detection(0, time_road0)
+        print "road0_num = ", road0_num
+        road1_num = detection(1, time_road1)
+        print "road1_num = ", road1_num
+        road2_num = detection(2, time_road2)
+        print "road2_num = ", road2_num
+        road3_num = detection(3, time_road3)
+        print "road3_num = ", road3_num
 
 
 if __name__ == "__main__":
